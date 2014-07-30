b7d07395e54d4e0ae2f67a75989b6e3028a76321
david liu <david.liu@autodesk.com>
2014-07-30 16:08:37 -0700

    asdf

--- mesos_cluster_client.py
+++ mesos_cluster_client.py
@@ -7,16 +7,19 @@ data = yaml.load(open('mesos.yaml', 'r'))
 port = 5050
 host = data['marathon']['host']
 etcd_host_address = data['etcd']['host']
-
+r = requests.get('http://'+host+':'+str(port)+'/state.json')
+state = json.loads(r.content)
 #
 # return a list of slave hosts
 #
 def get_slave_hosts():
-	r = requests.get('http://'+host+':'+str(port)+'/state.json')
-	state = json.loads(r.content)
+	# print 'http://'+host+':'+str(port)+'/state.json'
+	
 	slaves = state['slaves']
+	# print slaves
 	# print slaves.keys()
-	print map(lambda x: x['hostname'], slaves)
+	return map(lambda x: x['hostname'], slaves)
+	
 
 #
 # returns all running containers
@@ -27,5 +30,18 @@ def get_running_containers():
 # returns containers running on this host
 #
 def get_slave_containers(host):
-	print 'hji'
-get_slave_hosts()
+	# print 'doing this'
+	# r = requests.get('http://'+host+':'+str(port)+'/state.json')
+	# print 'good'
+	state = json.loads(r.content)
+	# slaves = state['slaves']
+	print state['slaves'][0].keys()
+	# return slaves
+
+
+if __name__ == "__main__":
+	slave_hosts = get_slave_hosts()
+	# print 'dont thiss'
+	# print slave_hosts
+	containers = get_slave_containers(slave_hosts[0])
+	print containers
